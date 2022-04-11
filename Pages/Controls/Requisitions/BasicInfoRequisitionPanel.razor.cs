9b9cfb6e56faae3aa5ceab350a58757f1a8c12c5
Mani Venkiteswaran <manivenkit@hotmail.com>
2022-04-11 15:22:21 +0530

    commit

--- Pages/Controls/Requisitions/BasicInfoRequisitionPanel.razor.cs
+++ Pages/Controls/Requisitions/BasicInfoRequisitionPanel.razor.cs
@@ -17,4 +17,43 @@ namespace ProfSvc_AppTrack.Pages.Controls.Requisitions;
 
 public partial class BasicInfoRequisitionPanel
 {
+    [Parameter]
+    public RequisitionDetails ModelObject
+    {
+        get;
+        set;
+    } = new();
+
+    private string GetDurationCode(string durationCode)
+    {
+        return durationCode.ToLower() switch
+               {
+                   "m" => "months",
+                   "w" => "weeks",
+                   "d" => "days",
+                   _ => "years"
+               };
+    }
+
+    [Parameter]
+    public List<IntValues> States
+    {
+        get;
+        set;
+    }
+
+    private string GetLocation(string location)
+    {
+        if (location.ToInt32() == 0)
+        {
+            return location;
+        }
+
+        foreach (IntValues _intValues in States.Where(intValues => location.ToInt32() == intValues.Key))
+        {
+            return _intValues.Value;
+        }
+
+        return location;
+    }
 }
