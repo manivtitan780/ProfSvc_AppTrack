0afabfa576e5c5e97aea85540336c11dfe3f28a6
Mani Venkiteswaran <manivenkit@hotmail.com>
2022-05-07 21:24:31 +0530

    commit

--- Pages/Controls/Requisitions/RequisitionDetailsPanel.razor.cs
+++ Pages/Controls/Requisitions/RequisitionDetailsPanel.razor.cs
@@ -2,5 +2,40 @@
 {
     public partial class RequisitionDetailsPanel
     {
+        public SfDialog Dialog
+        {
+            get;
+            set;
+        }
+
+        public SfSpinner Spinner
+        {
+            get;
+            set;
+        }
+
+        [Parameter]
+        public RequisitionDetails ModelObject
+        {
+            get;
+            set;
+        } = new();
+
+        [Parameter]
+        public List<KeyValues> Companies
+        {
+            get;
+            set;
+        }
+
+        private async Task SaveRequisitionDialog(EditContext arg)
+        {
+            await Task.Delay(1);
+        }
+
+        private async Task CancelDialog(MouseEventArgs arg)
+        {
+            await Task.Delay(1);
+        }
     }
 }
